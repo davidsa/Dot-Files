f68661a9ec237f122267de7712dc30d0733554be
David Sttivend <dsangel@gmail.com>
2022-05-19 00:25:17 -0500

    update rgb lights

--- keymap.c
+++ keymap.c
@@ -27,37 +27,74 @@
 // Light combinations
 #define SET_INDICATORS(hsv) \
 	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, hsv}
+    {36+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}
+
 #define SET_UNDERGLOW(hsv) \
-	{1, 6, hsv}, \
-    {35+1, 6,hsv}
+	{1   , 6, hsv}, \
+    {36+1, 6, hsv}
+
 #define SET_NUMROW(hsv) \
-	{10, 2, hsv}, \
-		{20, 2, hsv}, \
-		{30, 2, hsv}, \
-	  {35+ 10, 2, hsv}, \
-	  {35+ 20, 2, hsv}, \
-	  {35+ 30, 2, hsv}
-#define SET_INNER_COL(hsv)	\
-	{33, 4, hsv}, \
-	  {35+ 33, 4, hsv}
+	{12, 1, hsv}, \
+	{21, 2, hsv}, \
+	{31, 2, hsv}, \
+    {36+12, 1, hsv}, \
+    {36+21, 2, hsv}, \
+    {36+31, 2, hsv}
 
-#define SET_OUTER_COL(hsv) \
-	{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}
-#define SET_THUMB_CLUSTER(hsv) 	\
-	{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
-#define SET_LAYER_ID(hsv) 	\
-	{0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-    {35+0, 1, HSV_OVERRIDE_HELP(hsv, INDICATOR_BRIGHTNESS)}, \
-		{1, 6, hsv}, \
-    {35+1, 6, hsv}, \
-		{7, 4, hsv}, \
-	  {35+ 7, 4, hsv}, \
-		{25, 2, hsv}, \
-	  {35+ 25, 2, hsv}
+#define SET_MODIFIER_KEYS(hsv) \
+    {7, 2, hsv}, \
+    {16, 2, hsv}, \
+    {26, 1, hsv}, \
+    {36+7, 2, hsv}, \
+    {36+16, 2, hsv}, \
+    {36+26, 1, hsv}
 
+#define SET_THUMB_KEYS(hsv) \
+    {27, 1, hsv}, \
+    {36+27, 1, hsv}
+
+#define SET_REGULAR_KEYS(hsv) \
+    {9, 3, hsv}, \
+    {13, 3, hsv}, \
+    {18, 3, hsv}, \
+    {23, 3, hsv}, \
+    {28, 3, hsv}, \
+    {33, 3, hsv}, \
+    {9+13, 3, hsv}, \
+    {36+13, 3, hsv}, \
+    {36+18, 3, hsv}, \
+    {36+23, 3, hsv}, \
+    {36+28, 3, hsv}, \
+    {36+33, 3, hsv}
+
+#define SET_ESC(hsv) \
+    {9, 1, hsv}
+
+#define SET_SYMBOL_KEYS(hsv) \
+    {36+23, 3, hsv}, \
+    {36+28, 3, hsv}, \
+    {36+33, 3, hsv}
+
+#define SET_NAV(hsv) \
+    {36+34, 1, hsv}, \
+    {36+29, 1, hsv}, \
+    {36+24, 1, hsv}, \
+    {36+19, 1, hsv}
+
+#define SET_MEDIA(hsv) \
+    {23, 1, hsv}, \
+    {30, 1, hsv}, \
+    {33, 1, hsv}
+
+#define SET_TMUX(hsv) \
+    {19, 1, hsv}, \
+    {24, 1, hsv}, \
+    {29, 1, hsv}, \
+    {34, 1, hsv}
+
+#define SET_BRIGHTNESS(hsv) \
+    {25, 1, hsv}, \
+    {28, 1, hsv}
 
 enum sofle_layers {
     _DEFAULTS = 0,
@@ -187,7 +224,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   _______, _______,_TABCLOSE,_TABPREV,_TABNEXT,_TABNEW,                    _______, _______, _______, _______, _______, _______,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  _______, _______, _______, KC_BRID, KC_BRIU, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_BRID, KC_BRIU, RGB_TOG, _______,  _______, _______, _______, _______, _______, _______, _______,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
                  _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
@@ -223,65 +260,81 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef RGBLIGHT_ENABLE
 char layer_state_str[70];
-// Now define the array of layers. Later layers take precedence
 
-// QWERTY,
-// Light on inner column and underglow
 const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_RED)
-
+  SET_INDICATORS(HSV_BLUE),
+  SET_REGULAR_KEYS(HSV_BLUE),
+  SET_NUMROW(HSV_TURQUOISE),
+  SET_MODIFIER_KEYS(HSV_TEAL),
+  SET_ESC(HSV_TEAL),
+  SET_THUMB_KEYS(HSV_TEAL)
 );
-const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_PINK)
-);
 
-// _NUM,
-// Light on outer column and underglow
-const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_TEAL)
-
-);
-// _SYMBOL,
-// Light on inner column and underglow
 const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_BLUE)
-
-    );
-// _COMMAND,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-  SET_LAYER_ID(HSV_PURPLE)
+  SET_INDICATORS(HSV_PURPLE),
+  SET_REGULAR_KEYS(HSV_BLUE),
+  SET_NUMROW(HSV_TURQUOISE),
+  SET_MODIFIER_KEYS(HSV_TEAL),
+  SET_ESC(HSV_TEAL),
+  SET_THUMB_KEYS(HSV_TEAL),
+  SET_SYMBOL_KEYS(HSV_PURPLE)
 );
 
-// _SWITCHER   // light up top row
-const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	SET_LAYER_ID(HSV_GREEN),
-	SET_NUMROW(HSV_GREEN)
+const rgblight_segment_t PROGMEM layer_nav_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_INDICATORS(HSV_SPRINGGREEN),
+  SET_REGULAR_KEYS(HSV_BLUE),
+  SET_NUMROW(HSV_TURQUOISE),
+  SET_MODIFIER_KEYS(HSV_TEAL),
+  SET_ESC(HSV_TEAL),
+  SET_THUMB_KEYS(HSV_TEAL),
+  SET_NAV(HSV_SPRINGGREEN)
+);
+
+const rgblight_segment_t PROGMEM layer_raise_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_INDICATORS(HSV_ORANGE),
+  SET_REGULAR_KEYS(HSV_BLUE),
+  SET_NUMROW(HSV_TURQUOISE),
+  SET_MODIFIER_KEYS(HSV_TEAL),
+  SET_ESC(HSV_TEAL),
+  SET_THUMB_KEYS(HSV_TEAL),
+  SET_MEDIA(HSV_GREEN),
+  SET_TMUX(HSV_ORANGE),
+  SET_BRIGHTNESS(HSV_YELLOW),
+  {35, 1, HSV_RED}
+);
+
+const rgblight_segment_t PROGMEM layer_adjust_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+  SET_INDICATORS(HSV_BLUE),
+  SET_REGULAR_KEYS(HSV_BLUE),
+  SET_NUMROW(HSV_TURQUOISE),
+  SET_MODIFIER_KEYS(HSV_TEAL),
+  SET_ESC(HSV_TEAL),
+  SET_THUMB_KEYS(HSV_TEAL)
 );
 
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-
     layer_qwerty_lights,
-	layer_num_lights,// overrides layer 1
 	layer_symbol_lights,
-    layer_command_lights
+    layer_nav_lights,
+    layer_raise_lights,
+    layer_adjust_lights
 );
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
-
-
 	rgblight_set_layer_state(1, layer_state_cmp(state, _SYMBOLS));
-	rgblight_set_layer_state(2, layer_state_cmp(state, _RAISE));
-	rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
+	rgblight_set_layer_state(2, layer_state_cmp(state, _NAV));
+	rgblight_set_layer_state(3, layer_state_cmp(state, _RAISE));
+	rgblight_set_layer_state(4, layer_state_cmp(state, _ADJUST));
     return state;
 }
+
 void keyboard_post_init_user(void) {
     // Enable the LED layers
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_BLUE);
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
     rgblight_layers = my_rgb_layers;
-
-	rgblight_mode(10);// haven't found a way to set this in a more useful way
-
 }
 #endif
 
