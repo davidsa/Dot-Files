6d338f877c25a445b48e6e1cfdd2505adbd47b53
David Sttivend <dsangel@gmail.com>
2023-07-25 18:45:10 -0500

    Added fzf to projects and OpenGithubCurrentPR

--- nvim/lua/david/github.lua
+++ nvim/lua/david/github.lua
@@ -3,6 +3,15 @@ local git_url_cmd =
 
 local git_branch_cmd = "git branch | awk '{print $2}'"
 
+
+local find_hash_in_refs = function(branch)
+  return "git ls-remote origin | grep " .. branch .. " | awk '{print $1}'"
+end
+
+local find_pr_number_in_refs = function(hash)
+  return "git ls-remote origin | grep " .. hash .. " | awk 'FNR == 2 {print $2}' | awk -F '/' '{print $3}'"
+end
+
 local trim = function(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
@@ -26,6 +35,17 @@ end
 local get_line_number = function()
   local r, c = unpack(vim.api.nvim_win_get_cursor(0))
   return r
+end
+
+local open_github_current_pr = function()
+  local remote = run_cmd(git_url_cmd)
+  local branch = run_cmd(git_branch_cmd)
+  local hash = run_cmd(find_hash_in_refs(branch))
+  local pr_number = run_cmd(find_pr_number_in_refs(hash))
+
+  local url = remote .. "/pull/" .. pr_number
+
+  open_url(url)
 end
 
 local open_github_file = function()
@@ -55,3 +75,5 @@ end
 vim.api.nvim_create_user_command('OpenGithubFile', open_github_file, {})
 
 vim.api.nvim_create_user_command('OpenGithubPR', open_github_pr, {})
+
+vim.api.nvim_create_user_command('OpenGithubCurrentPR', open_github_current_pr, {})
