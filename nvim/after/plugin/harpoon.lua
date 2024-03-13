7ba0a93b762fcc1fc78d7689d89c8da50a3a8a40
David Sttivend <dsangel@gmail.com>
2024-03-13 13:19:11 -0500

    Updated keybindings for harpoon

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,3 +4,5 @@ local ui = require('harpoon.ui')
 vim.keymap.set('n', '<leader>a', mark.add_file)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 
+vim.keymap.set('n', '<C-S-k>', ui.nav_prev)
+vim.keymap.set('n', '<C-S-j>', ui.nav_next)
--- nvim/after/plugin/nerdtree.lua
+++ nvim/after/plugin/nerdtree.lua
@@ -1,2 +1,3 @@
 vim.g.NERDTreeWinSize = 40
 vim.g.NERDTreeMinimalMenu = 1
+vim.g.NERDTreeCaseSensitiveFS = 1
